d4b446e3cb6bc89d40b2a97bb72415bae977b4f3
ShaunFell <shaundbfell@gmail.com>
2024-04-05 18:59:06 +0200

    adding routine to plot pseudocolor plots

--- PostProcessing/PlotSliceRemix.py
+++ PostProcessing/PlotSliceRemix.py
@@ -1,62 +1,31 @@
 #
-## Fancier version of PlotSlice.py which makes a movie and uses
-## time stepping to go through the files (thanks to Tiago for the tips)
-## Run this using 'visit -nowin -cli -s PlotSliceRemix.py (path to hdf) (path to plot output)'
+## Run this using 'visit -nowin -cli -s PlotSliceRemix.py (path to hdf) (param file) (optional output directory)'
 ## to run in parallel do 'visit -np 32 -nowin -cli -s PlotSliceRemix.py'
-## NB you will need ffmpeg installed to make the movie
+## NOTE: you will need ffmpeg installed to make the movie
 #
 
-import glob, os, sys
+import glob, os, sys, configparser
+import numpy as np
 
-# --------------------------------------------------------------------------
-## Inputs - you need to update these!
-# --------------------------------------------------------------------------
-
-# file details
-plt_prefix = "GeneralizedProcap_"
-path_to_hdf5_files = sys.argv[1]
-
-# plot details
-# select variable 
-plot_variables = ["rho", "rhoE", "Ham", "Asquared"]
+## Load config file
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
 
 
-if len(sys.argv) >= 2:
-	output_directory = sys.argv[2]
-else:
-	output_directory = os.getcwd()
 
-if not os.path.exists(output_directory):
-	os.mkdir(output_directory)
-if not os.path.exists(output_directory+"../Movies"):
-	os.mkdir(output_directory+"../Movies")
+plotpath = config["Output"]["output_plot_path"]
+moviepath = config["Output"]["output_movie_path"]
 
-print("HDF5 file path: " + path_to_hdf5_files)
-print("Output directory: " + output_directory)
+if not os.path.exists(plotpath):
+	os.mkdir(plotpath)
+if not os.path.exists(moviepath):
+	os.mkdir(moviepath)
 
-# max and min values for colourbar
-set_min_max = 0 # 1 for true, 0 for false
-min_value = 0.0
-max_value = 1.0
-# slice origin and normal direction
-origin_point_x = 32
-origin_point_y = 32
-origin_point_z = 0
-normal_in_x = 0
-normal_in_y = 0
-normal_in_z = 1
-# max and min coords in the sliced plane (e.g. x and y if normal to z)
-# NB relative to origin as defined above
-min_u = -20
-max_u = 20
-min_v = min_u
-max_v = max_u
+print("HDF5 file path: " + config["Header"]["hdf5_path"])
+print("Output Plots directory: " +plotpath)
+print("Output Movie Directory: " +moviepath)
 
-# --------------------------------------------------------------------------
-# From here you only need to amend if you want to do something non standard
-# --------------------------------------------------------------------------
-
-def setup_slice_plot(variableToPlot) :
+def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	# annotation settings
 	AnnotationAtts = AnnotationAttributes()
 	AnnotationAtts.axes3D.visible = 1
@@ -87,11 +56,11 @@ def setup_slice_plot(variableToPlot) :
 	PseudocolorAtts.skewFactor = 1
      # limitsMode =  OriginalData, CurrentPlot
 	PseudocolorAtts.limitsMode = PseudocolorAtts.OriginalData
-	PseudocolorAtts.minFlag = set_min_max
-	PseudocolorAtts.min = min_value
-	PseudocolorAtts.maxFlag = set_min_max
-	PseudocolorAtts.max = max_value
-	PseudocolorAtts.colorTableName = "viridis"
+	PseudocolorAtts.minFlag = setplotbounds
+	PseudocolorAtts.min = plotbounds[0]
+	PseudocolorAtts.maxFlag = setplotbounds
+	PseudocolorAtts.max = setplotbounds[1]
+	PseudocolorAtts.colorTableName = config["PlotConfig"].get("colortable", fallback = "viridis")
 	PseudocolorAtts.invertColorTable = 0
      # opacityType = ColorTable, FullyOpaque, Constant, Ramp, VariableRange
 	PseudocolorAtts.opacityType = PseudocolorAtts.FullyOpaque 
@@ -105,10 +74,11 @@ def setup_slice_plot(variableToPlot) :
 	SliceAtts = SliceAttributes()
      # originType = Point, Intercept, Percent, Zone, Node
 	SliceAtts.originType = SliceAtts.Point  
-	SliceAtts.originPoint = (origin_point_x, origin_point_y, origin_point_z)
+	originpoint = np.float64(config["PlotConfig"]["origin"].split())
+	SliceAtts.originPoint = tuple(np.float64(config["PlotConfig"]["origin"].split()))
      # axisType = XAxis, YAxis, ZAxis, Arbitrary, ThetaPhi
 	SliceAtts.axisType = SliceAtts.Arbitrary  
-	SliceAtts.normal = (normal_in_x, normal_in_y, normal_in_z)
+	SliceAtts.normal =tuple(np.float64(config["PlotConfig"]["normal_vec"].split()))
 	SliceAtts.project2d = 1
 	SliceAtts.flip = 0
 	SetOperatorOptions(SliceAtts, 1)
@@ -123,7 +93,12 @@ def setup_slice_plot(variableToPlot) :
 	
 	# Zoom in
 	View2DAtts = View2DAttributes()
-	View2DAtts.windowCoords = (min_u, max_u, min_v, max_v)
+	View2DAtts.windowCoords = (
+		config["PlotConfig"]["plot_u_min"], 
+		config["PlotConfig"]["plot_u_max"],
+		config["PlotConfig"]["plot_v_min"],
+		config["PlotConfig"]["plot_v_max"]
+	)
 	View2DAtts.viewportCoords = (0.2, 0.95, 0.15, 0.95)
 	View2DAtts.fullFrameActivationMode = View2DAtts.Auto  # On, Off, Auto
 	View2DAtts.fullFrameAutoThreshold = 100
@@ -135,7 +110,7 @@ def setup_slice_plot(variableToPlot) :
 	# Save Me
 	SaveWindowAtts = SaveWindowAttributes()
 	SaveWindowAtts.outputToCurrentDirectory = 0
-	SaveWindowAtts.outputDirectory = output_directory
+	SaveWindowAtts.outputDirectory = plotpath
 	SaveWindowAtts.fileName = str(variableToPlot)
 	SaveWindowAtts.family = 1
   # format = BMP, CURVE, JPEG, OBJ, PNG, POSTSCRIPT, POVRAY, PPM, 
@@ -151,8 +126,9 @@ def setup_slice_plot(variableToPlot) :
 
 
 def PlotFiles():
-	files = glob.glob(path_to_hdf5_files + "*.3d.hdf5")
-	plot_files = [x for x in files if plt_prefix in x]
+	filename_prefix = os.path.join(config["Header"]["hdf5_path"], config["Header"]["plot_header"])
+	files = glob.glob(filename_prefix+ "*.3d.hdf5")
+	plot_files = [x for x in files if config["Header"]["plot_header"] in x]
 	return plot_files
 
 def MultipleDatabase():
@@ -164,21 +140,22 @@ def MultipleDatabase():
 		print("Opening single plot file")
 		return False
 
-def make_slice_plots(variableToPlot, hdf5files, hdf5files_base) :
+def make_slice_plots(variableToPlot, hdf5files, setplotbounds, plotbounds) :
 	if MultipleDatabase():
-		OpenDatabase(hdf5files_base + "*" + ".3d.hdf5 database", 0)
+		filename_prefix = os.path.join(config["Header"]["hdf5_path"], config["Header"]["plot_header"])
+		OpenDatabase(filename_prefix + "*" + ".3d.hdf5 database", 0)
 	else:
 		OpenDatabase(PlotFiles()[0], 0)
 
 	hdf5file = hdf5files[0]
 	print("Setup and plot first slice from file : " + hdf5file)
-	setup_slice_plot(variableToPlot)
+	setup_slice_plot(variableToPlot, plotbounds, setplotbounds)
 	for i in range(1, len(hdf5files)):
 		print("Plotting file " + hdf5files[i])
 		TimeSliderNextState()
 		SaveWindowAtts = SaveWindowAttributes()
 		SaveWindowAtts.outputToCurrentDirectory = 0
-		SaveWindowAtts.outputDirectory = output_directory
+		SaveWindowAtts.outputDirectory = config["Output"]["output_plot_path"]
 		SaveWindowAtts.fileName = str(variableToPlot) + ('%04d' % i)
 		SaveWindowAtts.family = 0 # needs to be enforced again
 		SetSaveWindowAttributes(SaveWindowAtts)	
@@ -188,18 +165,23 @@ def make_slice_plots(variableToPlot, hdf5files, hdf5files_base) :
 
 
 def main():
-	hdf5files_base = path_to_hdf5_files + plt_prefix
-	hdf5files = glob.glob(hdf5files_base + "*.hdf5")
+	hdf5files = glob.glob(config["Header"]["hdf5_path"] + config["Header"]["plot_header"] + "*.hdf5")
 	hdf5files.sort()
 	print("Running Plot routines!")
 
-	for plotvar in plot_variables:
+	plot_variables = config["VariableData"].get("plot_variables", "").split()
+	print("Number of plot variables: ", len(plot_variables))
+	print("Variables to be plotted: ", config["VariableData"].get("plot_variables", ""))
+	for i in range(len(plot_variables)):
+		plotvar = plot_variables[i]
 		print ("Plotting slices for {0}...".format(plotvar))
-		make_slice_plots(plotvar, hdf5files, hdf5files_base)
+		setplotbounds = config["VariableData"].get("set_plot_bounds_"+str(i), 0)
+		plotbounds = np.float64(np.array(config["VariableData"].get("plot_bounds_"+str(i), "0 0").split()))
+		make_slice_plots(plotvar, hdf5files, setplotbounds, plotbounds)
 
 		if MultipleDatabase():
 			print ("Making a movie...")
-			os.system('ffmpeg -r 5 -f image2 -s 1920x1080 -i ' + output_directory+str(plotvar) + '%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p ' + output_directory+"../Movies/"+str(plotvar) + '.mp4')
+			os.system('ffmpeg -r 5 -f image2 -s 1920x1080 -i ' + plotpath + '%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p ' +moviepath+ '.mp4')
 
 		print("I've finished!")
 	
