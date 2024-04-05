6e4962fb85f725ea02cce0dc5c0a74e7ebf40b8f
ShaunFell <shaundbfell@gmail.com>
2024-04-05 18:59:06 +0200

    adding routine to plot pseudocolor plots

--- PostProcessing/visitlog.py
+++
@@ -1,5 +0,0 @@
-# Visit 3.3.3 log file
-ScriptVersion = "3.3.3"
-if ScriptVersion != Version():
-    print "This script is for VisIt %s. It may not work with version %s" % (ScriptVersion, Version())
-ShowAllWindows()
