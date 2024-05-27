6a7315d3fcd36a10a869cc4365ec71b4e54c8efd
ShaunFell <shaundbfell@gmail.com>
2024-05-27 09:35:56 +0200

    clang format with version 13.0.0

--- Source/Core/runGRChombo.hpp
+++ Source/Core/runGRChombo.hpp
@@ -64,7 +64,8 @@ template <class level_t> int runGRChombo(int argc, char *argv[])
     if (sim_params.run_initial_posttimestep)
     { // call the PostTimeStep right now!!!!
 
-        auto task = [](GRAMRLevel *level) {
+        auto task = [](GRAMRLevel *level)
+        {
             if (level->time() == 0.)
             {
                 level->specificPostTimeStep();
