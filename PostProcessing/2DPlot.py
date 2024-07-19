1bf3898c753d063d87fc1fa3e782841505eb1b6e
Shaun Fell <37465343+ShaunFell@users.noreply.github.com>
2024-07-19 12:50:59 -0700

    added documentation for 2D plotter

--- PostProcessing/2DPlot.py
+++ PostProcessing/2DPlot.py
@@ -12,11 +12,12 @@ config = configparser.ConfigParser()
 config.read(sys.argv[1])
 
 
-
+# Extract config variables
 plotpath = config["Output"]["output_plot_path"]
 moviepath = config["Output"]["output_movie_path"]
 overwrite_plots = config["Output"].getboolean("overwrite_plots",0)
 
+#if the plot and movie paths dont exist, create them
 if not os.path.exists(plotpath):
 	os.mkdir(plotpath)
 if not os.path.exists(moviepath):
@@ -26,6 +27,7 @@ print("HDF5 file path: " + config["Header"]["hdf5_path"])
 print("Output Plots directory: " +plotpath)
 print("Output Movie Directory: " +moviepath)
 
+#set verbosity for printing
 def verbPrint(*objects):
 	if config["Header"].getint("verbosity",0):
 		print(*objects)
@@ -44,6 +46,7 @@ def setup_engine():
 		host = config["EngineConfig"].get("host", "localhost")
 		print("Running in parallel on host: ", host)
 	
+		# extract all the config options for the engine
 		num_procs = config["EngineConfig"].get("number_processes", 1)
 		num_nodes = config["EngineConfig"].get("number_nodes", 1)
 		partition_name = config["EngineConfig"].get("partition")
@@ -54,11 +57,22 @@ def setup_engine():
 
 		arg = ("-l", job_cmd, "-n", job_name, "-p", partition_name, "-np", num_procs, "-nn", num_nodes, "-t", time_limit, "-la", add_sub_args)
 
-		#Submit the job
+		#Submit the job using visit job scheduler functionality
 		OpenComputeEngine(host, arg)
 
+# This is where the real magic happens. This function creates a single slice plot.
 def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
-	# annotation settings
+	"""
+	Create all the data for a single slice plot
+	"""
+
+	"""
+	For documentation on all the attributes below, see
+		https://visit-sphinx-github-user-manual.readthedocs.io/en/develop/python_scripting/functions.html
+	"""
+
+
+	# annotation settings, such as the information printed on the plot (user, database name, time, etc.)
 	AnnotationAtts = AnnotationAttributes()
 	axes3dvisible = config["AnnotationConfig"].getint('axes3Dvisible', 1)
 	verbPrint("Axes3d visible: ", str(axes3dvisible))
@@ -81,7 +95,7 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	triadflag = config["AnnotationConfig"].getint('triadFlag', 0)
 	verbPrint("triadFlag: " , str(triadflag))
 
-
+	#Set the annotation attributes
 	AnnotationAtts.axes3D.visible = axes3dvisible
 	AnnotationAtts.userInfoFlag = userinfoflag
 	AnnotationAtts.databaseInfoFlag = databaseinfoflag
@@ -109,8 +123,8 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	
 	# add pseudocolour plot
 	AddPlot("Pseudocolor", variableToPlot, 1, 1)
-	PseudocolorAtts = PseudocolorAttributes()
-	plotscaling = config["PlotConfig"].get("plotscaling", fallback = "Linear").lower()
+	PseudocolorAtts = PseudocolorAttributes() #Get the pseudocolor plot attributes instance
+	plotscaling = config["PlotConfig"].get("plotscaling", fallback = "Linear").lower() #Determine how we want the plot ticks to be scaled.
 	if plotscaling == "linear":
 		PseudocolorAtts.scaling = PseudocolorAtts.Linear
 	elif plotscaling == "log":
@@ -126,11 +140,11 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	PseudocolorAtts.limitsMode = PseudocolorAtts.OriginalData
 	verbPrint("Setting plot bounds: ", setplotbounds)
 	verbPrint("Plot Bounds lower: ", plotbounds[0], " upper: ", plotbounds[1])
-	PseudocolorAtts.minFlag = setplotbounds
-	PseudocolorAtts.min = plotbounds[0]
-	PseudocolorAtts.maxFlag = setplotbounds
-	PseudocolorAtts.max = plotbounds[1]
-	PseudocolorAtts.colorTableName = config["PlotConfig"].get("colortable", fallback = "viridis") #Color table name
+	PseudocolorAtts.minFlag = setplotbounds #Do we want a minimum value?
+	PseudocolorAtts.min = plotbounds[0] #Set the minimum value
+	PseudocolorAtts.maxFlag = setplotbounds #Do we want a maximum value?
+	PseudocolorAtts.max = plotbounds[1] #Set the maximum value
+	PseudocolorAtts.colorTableName = config["PlotConfig"].get("colortable", fallback = "viridis") #The type of color data.
 	PseudocolorAtts.invertColorTable = config["PlotConfig"].getint("invert_color_table", fallback = 0) #Invert the color scale
      # opacityType = ColorTable, FullyOpaque, Constant, Ramp, VariableRange
 	PseudocolorAtts.opacityType = PseudocolorAtts.FullyOpaque 
@@ -144,13 +158,15 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	elif centering == "Zonal":
 		PseudocolorAtts.centering = PseudocolorAtts.Zonal
 
+	#Set the options we just created above to the pseudocolor plot
 	SetPlotOptions(PseudocolorAtts)
 	
+
 	# slice the pseudocolour plot
 	AddOperator("Slice", 1)
 	SliceAtts = SliceAttributes()
 
-	origintype = config["SliceConfig"].get("origin_type", fallback = "Intercept")
+	origintype = config["SliceConfig"].get("origin_type", fallback = "Intercept") #Set how we want to slice the plot. Point, Intercept, Percent, Zone, Node
 	verbPrint("Origin type: ", origintype)
 
 	if origintype=="Point":
@@ -180,14 +196,16 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	elif axistype == "thetaphi":
 		SliceAtts.axisType = SliceAtts.ThetaPhi
 
-	#normal vector for slice surface
+	#normal vector for the plane that slices the pseudocolor plot
 	slicenormalvec = tuple(np.float64(config["SliceConfig"]["normal_vec"].split()))
 	verbPrint("Normal vector: ", slicenormalvec)
-	SliceAtts.normal = slicenormalvec
-	SliceAtts.project2d = 1
+	SliceAtts.normal = slicenormalvec #The slice plane's normal vector
+	SliceAtts.project2d = 1 #Do we project the plot to 2D or keep the sliced surface in a 3D plot?
 	SliceAtts.flip = 0
 	SetOperatorOptions(SliceAtts, 1)
 
+
+	#Should we plot the mesh as well?
 	if config["MeshConfig"].getboolean("activatemesh", 0):
 		AddPlot("Mesh", "Mesh",1,1)
 		#Mesh attributes
@@ -207,6 +225,9 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	# Ok, draw the plot now!
 	DrawPlots()
 	
+
+	## Camera Settings
+
 	# set the zoom
 	View2DAtts = View2DAttributes()
 	windowcoords = (
@@ -251,6 +272,7 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	SaveWindowAtts.fileName = str(variableToPlot)
 	SaveWindowAtts.family = 1
   
+	# Set the file format
 	outformat = config["Output"].get("fileform", fallback = "PNG").upper()
 	if outformat == "png":
 			SaveWindowAtts.format = SaveWindowAtts.PNG
@@ -280,6 +302,7 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 			SaveWindowAtts.format = SaveWindowAtts.PLY
 
 
+	# Set the resolution
 	SaveWindowAtts.width = np.float64(config["Output"].get("width", fallback = 1024))
 	SaveWindowAtts.height = np.float64(config["Output"].get("height", fallback = 1024))
 	SaveWindowAtts.quality = np.float64(config["Output"].get("quality", fallback = 80))
@@ -287,6 +310,7 @@ def setup_slice_plot(variableToPlot, plotbounds, setplotbounds) :
 	SaveWindowAtts.resConstraint = SaveWindowAtts.NoConstraint 
 	SetSaveWindowAttributes(SaveWindowAtts)
 	SaveWindow()
+
 
 def PlotFiles():
 	"""
@@ -300,7 +324,8 @@ def PlotFiles():
 	return plot_files
 
 def MultipleDatabase():
-	""" Flag that tells us if theres multiple files
+	""" 
+	Flag that tells us if there's multiple files
 	"""
 	
 	if len(PlotFiles())>1:
@@ -311,7 +336,8 @@ def MultipleDatabase():
 		return False
 
 def make_slice_plots(variableToPlot, hdf5files, setplotbounds, plotbounds) :
-	""" Do that actual plotting, iterating over all the files for a given variable
+	""" 
+	Do that actual plotting, iterating over all the files for a given variable
 	"""
 
 	# open all the hdf5 files.
